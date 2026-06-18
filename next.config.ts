440860ba76e2b429802594cedcf461662a10fd1e
Camila Chung <mila_chan@msn.com>
2026-06-17 22:47:10 -0300

    fix: adiciona basePath para o github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', 
+  basePath: '/Prototipo_MapaGeoespacial_SP',
   images: {
     unoptimized: true, 
   },
